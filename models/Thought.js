5e8c0a4704c52d10dd842e5936f93fde706be5bd
octofoxx <devilmaycry87x@gmail.com>
2023-09-10 18:22:08 -0700

    put in toLocaleString to format date maybe?

--- models/Thought.js
+++ models/Thought.js
@@ -21,6 +21,7 @@ const ReactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get:(timeStamp) => new Date(timeStamp).toLocaleString(),
     },
   },
   {
@@ -43,6 +44,7 @@ const ThoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get:(timeStamp) => new Date(timeStamp).toLocaleString(),
     },
 
     username: {
@@ -53,7 +55,7 @@ const ThoughtSchema = new Schema(
   },
   {
     toJSON: {
-      virtuals: true,
+      getters: true,
     },
     id: false,
   }
